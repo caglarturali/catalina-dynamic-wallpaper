7d9016805aa702085176e30a6fa751ddff9a4894
Caglar Turali <caglarturali@gmail.com>
2019-07-05 23:21:23 +0300

    Initial commit :tada:

---
+++ install.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+###
+# Installs Catalina Dynamic Wallpaper.
+##
+
+_THEMES_DIR="${HOME}/.local/share/wallpapers"
+_AUTOSTART_DIR="${HOME}/.config/autostart-scripts"
+
+echo "Installing Catalina Dynamic Wallpaper..."
+
+# Copy theme.
+mkdir -p $_THEMES_DIR
+cp -r CatalinaDynamic $_THEMES_DIR
+
+# Copy scripts
+cp -r bin ${HOME}
+chmod +x ${HOME}/bin/*
+# Create symlink under autostart-scripts.
+ln -sf ${HOME}/bin/catalina_dynamic $_AUTOSTART_DIR
+# Run for the first time.
+${HOME}/bin/catalina_dynamic &
+
+echo "Installation complete."
